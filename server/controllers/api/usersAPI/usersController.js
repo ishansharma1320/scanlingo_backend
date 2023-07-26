d3de28b34831bc4fc9d6539393ab16ce04d59e69
Ishan Sharma <ishansharma1320@gmail.com>
2023-07-26 00:03:09 -0300

    User API complete

--- server/controllers/api/usersAPI/usersController.js
+++ server/controllers/api/usersAPI/usersController.js
@@ -1,12 +1,74 @@
 const {db} = require("../../../../firebase");
 const getUsersData = async (req,res)=>{
-    console.log(req.currentUser);
-    const snapshot = await db.collection('users').get();
-    snapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-    });
+    let code = 200;
+    let response = {status: false, response: null}
 
-    res.json({"success":true})
+    console.log(req.currentUser);
+    try{
+        const userRef = await db.collection('users').doc(req.currentUser.uid);
+        const doc = await userRef.get();
+        if(!doc.exists){
+            response["error"] = "No User found";
+
+            code = 404
+        } else {
+            response.response = {message: doc.data()};            
+            code = 200
+        }
+        response.status = true;
+    } catch(err){
+        console.error(err)
+        response["error"] = err.message;
+    }
+    res.status(code).json(response)
 }
 
+
+
+const createUserData = async(req, res)=>{
+    let code = 201;
+    let response = {status: false, response: null}
+    const {firstName, lastName, email } = req.body;
+    console.log(req.currentUser);
+
+    try{
+        let userData = {user_id: req.currentUser.uid, firstName, lastName, email};
+        await db.collection('users').doc(req.currentUser.uid).set(userData);
+        response.status =true;
+        response.response = {message: userData}
+    } catch(err){
+        console.error(err)
+        response["error"] = err.message;
+        code = 500;
+    }
+
+    res.status(code).json(response)
+}
+
+const updateUserData = async (req,res)=>{
+    let code = 201;
+    let response = {status: false, response: null};
+    const {firstName, lastName} = req.body;
+
+    try{
+        let userData = {firstName, lastName};
+        await db.collection('users').doc(req.currentUser.uid).update(userData);
+        response.status =true;
+        response.response = {message: {user_id: req.currentUser.uid,...userData}}
+    } catch(err){
+        console.error(err)
+        response["error"] = err.message;
+        code = 500;
+    }
+
+    res.status(code).json(response)
+}
 module.exports.getUsersData =getUsersData;
+module.exports.createUserData = createUserData;
+module.exports.updateUserData = updateUserData;
+// getUserData
+// createUser
+// uploadImage (background upload to S3) // triggers textract and stores to DB
+// uploading to S3 creates a thumbnail for images and creates URL stores them to DB
+// text translate
+// CRD for bookmarks
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,8 @@
 const usersRoutes = require("./controllers/api/usersAPI/usersController");
 
 module.exports.register = function (router){
-    router.use("/users",usersRoutes.getUsersData)
+    router.get("/user",usersRoutes.getUsersData);
+    router.post("/user",usersRoutes.createUserData);
+    router.put("/user",usersRoutes.updateUserData);
 }
+
